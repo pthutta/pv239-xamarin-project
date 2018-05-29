496a94fdc205e18048268663b4006b023075d2ff
davidcechak <cechyy@gmail.com>
2018-05-29 12:40:52 +0200

    Fixing RoleInfoInitPage32

--- Triple_Eater/Triple_Eater/Pages/RoleChoice/RoleInfoInitPage31.xaml.cs
+++ Triple_Eater/Triple_Eater/Pages/RoleChoice/RoleInfoInitPage31.xaml.cs
@@ -10,8 +10,8 @@ using Xamarin.Forms.Xaml;
 namespace Triple_Eater.Pages.RoleChoice
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class RoleInfoInitPage31 : ContentPage
-	{
+	public partial class RoleInfoInitPage31 : BasePage<RoleInfoPage32>
+    {
 		public RoleInfoInitPage31 ()
 		{
 			InitializeComponent ();
