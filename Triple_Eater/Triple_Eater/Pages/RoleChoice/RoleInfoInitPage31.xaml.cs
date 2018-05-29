2a3b8083d34b756a2d4e0ac10ebcece50a7990f8
davidcechak <cechyy@gmail.com>
2018-05-29 19:30:04 +0200

    Add stuff to RoleInfoInitPage31 and RoleInfoPage32

--- Triple_Eater/Triple_Eater/Pages/RoleChoice/RoleInfoInitPage31.xaml.cs
+++ Triple_Eater/Triple_Eater/Pages/RoleChoice/RoleInfoInitPage31.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Triple_Eater.DataModels;
+using Triple_Eater.Pages.Actions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,15 +14,62 @@ namespace Triple_Eater.Pages.RoleChoice
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RoleInfoInitPage31 : ContentPage
     {
-		public RoleInfoInitPage31 ()
-		{
-			InitializeComponent ();
+
+        private ObservableCollection<Player> _players = new ObservableCollection<Player>();
+        private Player _currentPlayer;
+
+        public ObservableCollection<Player> Players
+        {
+            get => _players;
+            set
+            {
+                _players = value;
+                OnPropertyChanged();
+            }
         }
 
-        public void NextPageButton_OnClicked(object sender, EventArgs e)
+        public Player CurrentPlayer
         {
-            var nextPage = new NavigationPage(new RoleInfoPage32());
-            NavigationPage.SetHasNavigationBar(nextPage, false);
+            get => _currentPlayer;
+            set
+            {
+                _currentPlayer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RoleInfoInitPage31()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        protected async override void OnAppearing()
+        {
+            Players = new ObservableCollection<Player>(
+                    await App.Database.TryGetAllPlayersAsync()
+            );
+            base.OnAppearing();
+
+            _currentPlayer = Players.Where((x) => !x.WasProcessed).FirstOrDefault();
+        }
+
+        public async Task NextPageButton_OnClickedAsync(object sender, EventArgs e)
+        {
+            _currentPlayer.WasProcessed = true;
+            await App.Database.TryUpdatePlayerAsync(_currentPlayer);
+
+            NavigationPage nextPage = null;
+            if (Players.Where((x) => !x.WasProcessed).Count() == 0)
+            {
+                nextPage = new NavigationPage(new RoleInfoInitPage31());
+            }
+            else
+            {
+                nextPage = new NavigationPage(new ActionPhaseInfoPage4());
+            }
+
+            NavigationPage.SetHasNavigationBar(nextPage, false); 
             Application.Current.MainPage?.Navigation.PushAsync(nextPage);
         }
     }
--- Triple_Eater/Triple_Eater/Pages/RoleChoice/RoleInfoPage32.xaml.cs
+++ Triple_Eater/Triple_Eater/Pages/RoleChoice/RoleInfoPage32.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Triple_Eater.DataModels;
 using Triple_Eater.Pages.Actions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +13,23 @@ namespace Triple_Eater.Pages.RoleChoice
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RoleInfoPage32 : ContentPage
 	{
-		public RoleInfoPage32 ()
-		{
+        private Player _player;
+
+        public Player Player
+        {
+            get => _player;
+            set
+            {
+                _player = value;
+                OnPropertyChanged();
+            }
+                
+        }
+
+		public RoleInfoPage32(Player player)
+        {
 			InitializeComponent ();
+            _player = player;
 		}
 
         public void NextPageButton_OnClicked(object sender, EventArgs e)
