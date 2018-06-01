e93126b9475d13bc283d8ec047b2a364284f454d
Peter Hutta <hutta.peto@gmail.com>
2018-06-01 15:47:00 +0200

    Add styles for page 1 and 2

--- Triple_Eater/Triple_Eater/Pages/MainPage.xaml.cs
+++ Triple_Eater/Triple_Eater/Pages/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -79,6 +80,12 @@ namespace Triple_Eater.Pages
         {
             DependencyService.Get<IMinimizeAppService>().Minimize();
             return true;
+        }
+
+        private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var listView = (ListView) sender;
+            listView.SelectedItem = null;
         }
     }
 }
